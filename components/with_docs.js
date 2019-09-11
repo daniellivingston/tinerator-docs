39ec1458c37e6f12076f6b6b9b37d1ef4f1a051b
Derrick Reimer <derrickreimer@gmail.com>
2019-09-11 15:03:48 -0500

    Make docs header sticky, add some more content

--- components/with_docs.js
+++ components/with_docs.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Header from './header';
 import Footer from './footer';
 import OpenGraph from './open_graph';
@@ -9,6 +9,8 @@ import Prism from 'prismjs';
 import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-diff';
 
+import useInterval from './use_interval';
+
 const components = {
   pre: props => {
     useEffect(() => {
@@ -17,6 +19,20 @@ const components = {
 
     return <pre {...props} />;
   }
+};
+
+const HeaderWrapper = props => {
+  const injectedClasses = props.className || '';
+  const [scrolledTop, setScrolledTop] = useState(true);
+  const scrollClass = scrolledTop ? '' : 'shadow-lg';
+
+  useInterval(() => {
+    setScrolledTop(window.scrollY == 0);
+  }, 100);
+
+  return (
+    <div className={`${injectedClasses} ${scrollClass}`}>{props.children}</div>
+  );
 };
 
 export default meta => ({ children }) => {
@@ -29,14 +45,20 @@ export default meta => ({ children }) => {
             description={meta.description}
             path={meta.path}
           />
-          <Header pageTitle={meta.title} />
+          <HeaderWrapper className="fixed top-0 left-0 right-0 bg-white z-20">
+            <Header pageTitle={meta.title} />
+          </HeaderWrapper>
 
-          <div className="mx-auto px-6 py-12 container sm:flex">
-            <div className="flex-shrink-0 pb-12 w-48 lg:w-64">
-              <Nav currentPath={meta.path} />
+          <div className="mx-auto px-6 pt-32 pb-12 container sm:flex">
+            <div className="flex-shrink-0 relative pb-12 w-48 lg:w-64">
+              <div className="sm:fixed">
+                <Nav currentPath={meta.path} />
+              </div>
             </div>
-            <div className="flex-grow-1 min-w-0 markdown max-w-2xl">
-              {children}
+            <div className="flex flex-grow-1 min-w-0">
+              <div className="flex-grow-1 min-w-0 markdown max-w-2xl">
+                {children}
+              </div>
             </div>
           </div>
         </main>
