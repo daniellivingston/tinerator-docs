278cf493d8f60b1947d28f08561dc649f246dfaa
Derrick Reimer <derrickreimer@gmail.com>
2019-09-23 09:03:51 -0500

    Refactor feature grid

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Footer from '../components/footer';
 import OpenGraph from '../components/open_graph';
 import ReactDemo from '../components/react_demo';
 import StandaloneDemo from '../components/standalone_demo';
+import FeatureGrid from '../components/feature_grid';
 
 const checkIcon = `
 <svg width="31px" height="31px" viewBox="0 0 31 31" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -126,101 +127,7 @@ function HomePage() {
             </div>
           </div>
 
-          <div className="pb-24">
-            <h2 className="px-6 pb-8 text-4xl lg:text-5xl leading-tight tracking-tight font-bold">
-              Batteries Included
-            </h2>
-
-            <div className="flex flex-wrap">
-              <div className="px-6 py-4 sm:py-8 sm:w-1/2 lg:w-1/4">
-                <h3 className="pb-3 text-2xl leading-tight tracking-tight font-semibold">
-                  Built-In React Support
-                </h3>
-
-                <p className="text-gray-700">
-                  Our <code className="inline-code">useForm</code> hook works
-                  great with Next.js, Gatsby, and other React environments.
-                </p>
-              </div>
-
-              <div className="px-6 py-4 sm:py-8 sm:w-1/2 lg:w-1/4">
-                <h3 className="pb-3 text-2xl leading-tight tracking-tight font-semibold">
-                  Storage and Exports
-                </h3>
-
-                <p className="text-gray-700">
-                  We store your submissions for you and allow you to export them
-                  to CSV.
-                </p>
-              </div>
-
-              <div className="px-6 py-4 sm:py-8 sm:w-1/2 lg:w-1/4">
-                <h3 className="pb-3 text-2xl leading-tight tracking-tight font-semibold">
-                  Spam Protection
-                </h3>
-
-                <p className="text-gray-700">
-                  Our proprietary spam algorithm keeps bots away, without
-                  subjecting users to reCAPTCHA.
-                </p>
-              </div>
-
-              <div className="px-6 py-4 sm:py-8 sm:w-1/2 lg:w-1/4">
-                <h3 className="pb-3 text-2xl leading-tight tracking-tight font-semibold">
-                  Email Notifications
-                </h3>
-
-                <p className="text-gray-700">
-                  Configure one or more email notifications any time your form
-                  is submitted.
-                </p>
-              </div>
-
-              <div className="px-6 py-4 sm:py-8 sm:w-1/2 lg:w-1/4">
-                <h3 className="pb-3 text-2xl leading-tight tracking-tight font-semibold">
-                  Server-Side Rules
-                </h3>
-
-                <p className="text-gray-700">
-                  Ensure invalid submissions don't slip through by setting rules
-                  on the server-side.
-                </p>
-              </div>
-
-              <div className="px-6 py-4 sm:py-8 sm:w-1/2 lg:w-1/4">
-                <h3 className="pb-3 text-2xl leading-tight tracking-tight font-semibold">
-                  Field Whitelisting
-                </h3>
-
-                <p className="text-gray-700">
-                  Block unwanted fields from coming through, to keep your
-                  dataset squeaky clean.
-                </p>
-              </div>
-
-              <div className="px-6 py-4 sm:py-8 sm:w-1/2 lg:w-1/4">
-                <h3 className="pb-3 text-2xl leading-tight tracking-tight font-semibold">
-                  Zapier Integration
-                </h3>
-
-                <p className="text-gray-700">
-                  Forward your form submissions anywhere with our first-class
-                  Zapier integration.
-                </p>
-              </div>
-
-              <div className="px-6 py-4 sm:py-8 sm:w-1/2 lg:w-1/4">
-                <h3 className="pb-3 text-2xl leading-tight tracking-tight font-semibold">
-                  Webhooks
-                </h3>
-
-                <p className="text-gray-700">
-                  POST to an HTTP endpoint of your choosing any time your form
-                  submitted.
-                </p>
-              </div>
-            </div>
-          </div>
+          <FeatureGrid />
         </div>
       </main>
 
