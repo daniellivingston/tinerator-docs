201ddb6022d2d594d6b101d3f667af2d9ec0b97d
Derrick Reimer <derrickreimer@gmail.com>
2019-11-26 14:25:39 -0600

    Flesh out mailchimp guide

--- components/step_heading.js
+++ components/step_heading.js
@@ -1,6 +1,6 @@
 export default props => (
   <div className="flex items-center">
-    <div className="flex items-center justify-center mr-3 text-lg bg-green-200 w-10 h-10 rounded-full font-bold text-green-700">
+    <div className="flex items-center justify-center mr-3 bg-green-200 w-10 h-10 font-semibold text-green-800">
       {props.step}
     </div>
     <div>{props.children}</div>
