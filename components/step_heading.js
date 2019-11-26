c1e0a1ece8a4b656c32a2d94c7e078bdc66635a5
Derrick Reimer <derrickreimer@gmail.com>
2019-11-26 16:50:09 -0600

    Add touch to step heading number

--- components/step_heading.js
+++ components/step_heading.js
@@ -1,6 +1,6 @@
 export default props => (
   <div className="flex items-center">
-    <div className="flex items-center justify-center mr-3 text-lg bg-green-200 w-8 h-8 font-bold text-green-800">
+    <div className="flex items-center justify-center mr-3 text-lg bg-green-200 w-8 h-8 font-bold text-green-800 border-b-2 border-green-500">
       {props.step}
     </div>
     <div>{props.children}</div>
