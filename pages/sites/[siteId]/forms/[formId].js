a1c7e75123c9b105737a235210837cf2cd99cb50
Derrick Reimer <derrickreimer@gmail.com>
2019-12-18 10:59:42 -0600

    Add submission deletion

--- pages/sites/[siteId]/forms/[formId].js
+++ pages/sites/[siteId]/forms/[formId].js
@@ -6,11 +6,25 @@ import Header from '../../../../components/header';
 import OpenGraph from '../../../../components/open_graph';
 import SiteContext from '../../../../components/site_context';
 import { getToken, redirectToLogin } from '../../../../utils/auth';
+import graphql from '../../../../utils/graphql';
 import { useViewer, fetch as fetchViewer } from '../../../../data/viewer';
 import { useSite, fetch as fetchSite } from '../../../../data/site';
 import { useForm, fetch as fetchForm } from '../../../../data/form';
 import { useSubmissions } from '../../../../data/submissions';
 import moment from 'moment';
+
+const trashIcon = `
+<svg width="19px" height="21px" viewBox="0 0 19 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+        <g id="trash-2" transform="translate(0.500000, 0.500000)" stroke="#E53E3E">
+            <polyline id="Path" points="0 4 2 4 18 4"></polyline>
+            <path d="M16,4 L16,18 C16,19.1045695 15.1045695,20 14,20 L4,20 C2.8954305,20 2,19.1045695 2,18 L2,4 M5,4 L5,2 C5,0.8954305 5.8954305,0 7,0 L11,0 C12.1045695,0 13,0.8954305 13,2 L13,4" id="Shape"></path>
+            <path d="M7,9 L7,15" id="Path"></path>
+            <path d="M11,9 L11,15" id="Path"></path>
+        </g>
+    </g>
+</svg>
+`;
 
 const formatFieldValue = value => {
   if (!value) return '—';
@@ -21,7 +35,9 @@ const formatFieldValue = value => {
     .join(' ');
 };
 
-const SubmissionTable = ({ submissionData, displayFields }) => {
+const SubmissionTable = ({ form, submissionData, displayFields }) => {
+  const router = useRouter();
+
   if (!submissionData) {
     return (
       <div className="mx-auto container px-6 py-24 text-2xl font-semibold text-gray-500 text-center">
@@ -32,11 +48,46 @@ const SubmissionTable = ({ submissionData, displayFields }) => {
 
   const submissions = submissionData.submissions.edges.map(edge => edge.node);
 
+  const deleteClicked = async id => {
+    const token = getToken();
+
+    const query = `
+      mutation DeleteFormSubmission(
+        $formId: ID!,
+        $submissionId: ID!
+      ) {
+        deleteFormSubmission(
+          formId: $formId,
+          submissionId: $submissionId
+        ) {
+          success
+          errors {
+            field
+            message
+          }
+          submission {
+            id
+          }
+        }
+      }
+    `;
+
+    const variables = { formId: form.id, submissionId: id };
+
+    try {
+      const resp = await graphql(query, variables, token);
+
+      if (resp.ok) {
+        router.reload();
+      }
+    } catch (e) {}
+  };
+
   return (
     <div className="overflow-auto w-full rounded-t-lg">
       <table className="w-full text-gray-800 text-sm">
         <thead>
-          <tr className="">
+          <tr>
             <th className="px-3 py-3 text-left font-semibold bg-gray-200 text-gray-700 border-b rounded-tl-lg">
               created_at
             </th>
@@ -67,7 +118,14 @@ const SubmissionTable = ({ submissionData, displayFields }) => {
                   </td>
                 ))}
 
-                <td className="px-3 py-4 w-12"></td>
+                <td className="px-3 py-4 w-12">
+                  <button
+                    className="flex items-center justify-center"
+                    onClick={() => deleteClicked(submission.id)}
+                  >
+                    <div dangerouslySetInnerHTML={{ __html: trashIcon }} />
+                  </button>
+                </td>
               </tr>
             );
           })}
@@ -159,6 +217,7 @@ function FormPage({
             </div>
 
             <SubmissionTable
+              form={form}
               submissionData={submissionData}
               displayFields={form.displayFields}
             />
