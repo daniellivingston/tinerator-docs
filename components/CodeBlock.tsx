ca42e7a852a6990d7e342ddacb8b2b57adae05b4
Derrick Reimer <derrickreimer@gmail.com>
2020-01-15 15:36:47 -0600

    Add copy button to code blocks

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Highlight, { Language, defaultProps } from 'prism-react-renderer';
 import { useDefaultSite } from 'utils/default-site';
 import { useSiteData } from 'data/site';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+const copyIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
+`;
 
 interface Props {
   className: string;
@@ -30,23 +35,31 @@ const CodeBlock: React.FC<Props> = ({ children, className, trim }) => {
   }, [siteId, siteData]);
 
   return (
-    <Highlight {...defaultProps} theme={null} code={code} language={language}>
-      {({ className, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className}>
-          {tokens.map((line, i) => {
-            if (trim && i === tokens.length - 1) return <></>;
+    <div className="code-block relative">
+      <Highlight {...defaultProps} theme={null} code={code} language={language}>
+        {({ className, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className}>
+            {tokens.map((line, i) => {
+              if (trim && i === tokens.length - 1) return <></>;
 
-            return (
-              <div key={i} {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            );
-          })}
-        </pre>
-      )}
-    </Highlight>
+              return (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              );
+            })}
+          </pre>
+        )}
+      </Highlight>
+
+      <CopyToClipboard text={code}>
+        <button className="absolute top-0 right-0 flex p-3 text-gray-300">
+          <span dangerouslySetInnerHTML={{ __html: copyIcon }} />
+        </button>
+      </CopyToClipboard>
+    </div>
   );
 };
 
