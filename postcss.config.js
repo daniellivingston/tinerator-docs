a0b51a72319631895ea9af21ed0d544814936f0e
Derrick Reimer <derrickreimer@gmail.com>
2020-02-10 16:03:40 -0600

    Disable purging for the moment

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === `production`
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: [
-              './components/**/*.tsx',
-              './pages/**/*.tsx',
-              './utils/**/*.tsx'
-            ],
-            css: ['../styles/main.css'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-          }
-        }
-      : {})
+    autoprefixer: {}
   }
 };
