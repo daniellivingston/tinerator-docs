219c1d86953768ba3c2bf8416539b8bfd481cdd8
Derrick Reimer <derrickreimer@gmail.com>
2020-02-10 15:15:01 -0600

    Update tailwind importing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const slug = require('rehype-slug');
 const link = require('rehype-autolink-headings');
-const withCSS = require('@zeit/next-css');
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
@@ -9,17 +8,15 @@ const withMDX = require('@next/mdx')({
 });
 const path = require('path');
 
-module.exports = withMDX(
-  withCSS({
-    target: 'serverless',
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
-    webpack(config, options) {
-      // Allow absolute imports of common paths
-      // See https://github.com/zeit/next.js/blob/master/examples/with-absolute-imports/next.config.js
-      ['components', 'utils', 'data'].forEach(dir => {
-        config.resolve.alias[dir] = path.join(__dirname, dir);
-      });
-      return config;
-    }
-  })
-);
+module.exports = withMDX({
+  target: 'serverless',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
+  webpack(config, options) {
+    // Allow absolute imports of common paths
+    // See https://github.com/zeit/next.js/blob/master/examples/with-absolute-imports/next.config.js
+    ['components', 'utils', 'data'].forEach(dir => {
+      config.resolve.alias[dir] = path.join(__dirname, dir);
+    });
+    return config;
+  }
+});
